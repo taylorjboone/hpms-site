4225522dff70a3fd751f611ad3af1d675ef64c9f
KamenRiderRyuki <geekdomfan@yahoo.com>
2022-08-03 08:24:46 -0400

    Add zip function
    Added zip capabilities and allow for batch uploading to website

--- __init__.py
+++ __init__.py
@@ -1,11 +1,15 @@
 from fileinput import filename
 import os
-from flask import request,redirect, url_for,send_from_directory, send_file
-from flask import Flask,current_app,render_template,flash
+from os import listdir
+from os.path import isfile, join
+# from flask import request,redirect, url_for,send_from_directory, send_file
+from flask import Flask,current_app,render_template,flash,request,redirect, url_for,send_from_directory, send_file
 import json
 from werkzeug.utils import secure_filename
 from io import StringIO
 import pandas as pd
+from zipfile import ZipFile
+from glob import glob
 
 from . import rptconverter as rpt
 
@@ -79,7 +83,34 @@ def hpms_dashboard():
 
 @app.route('/geo_counts_converter/convert',methods=['POST'])
 def geo_counts_convert():
-    f=request.files['filename']
-    f.save(r'static\geo_count_conversion\b.txt')
-    gc.geo_count_process(r'static\geo_count_conversion\b.txt')
-    return send_file(r'static\geo_count_conversion\geo_count_out.xlsx')
+    cent_number=1
+    metro_number=1
+    meh=request.files.getlist("file[]")
+    file_list_in=[]
+    file_list_out=[]
+    for file in meh:
+        new_path = r'static\geo_count_conversion\%s' % file.filename
+        file.save(new_path)
+        cent_number,metro_number,out_file=gc.geo_count_process(new_path,cent_number,metro_number)
+        # ZipFile.write(r'static\geo_count_conversion\geo_count_out.xlsx',arcname=r'static\geo_count_conversion\zip_geo_count.zip')
+        file_list_in.append(new_path)
+        if out_file != '':
+            file_list_out.append(out_file)
+        print(file_list_in,file_list_out)
+    # mypath=r'static\geo_count_conversion'
+    # onlyfiles = [os.path.join(mypath,f) for f in listdir(mypath) if isfile(join(mypath, f)) if f.endswith('.xlsx')]
+    # print(onlyfiles)
+    
+    with ZipFile(r'static\geo_count_conversion\zip_geo_count.zip','w') as zpf:
+        for file in file_list_out:
+            zpf.write(file)
+    zp=zip(file_list_in,file_list_out)
+    for a,b in zp:
+        os.remove(a)
+        os.remove(b)
+    return send_file(r'static\geo_count_conversion\zip_geo_count.zip')
+
+# def upload():
+#     uploaded_files = Flask.request.files.getlist("file[]")
+#     print(uploaded_files)
+#     return ""
--- geo_counts.py
+++ geo_counts.py
@@ -5,8 +5,9 @@ import os
 import openpyxl
 
 
-mypath='C:\PythonTest\Voltron\district_chrystal_report_website\hpms-site\geo_count_home_folder'
+mypath='C:\PythonTest\Voltron\district_chrystal_report_website\hpms-site\static\geo_count_conversion'
 number=1
+
 # geo=pd.read_csv('C:/PythonTest/Voltron/district_chrystal_report_website/hpms-site/013001_202109210000_0 (1).txt',skiprows=16,sep=';',header=None)
 
 
@@ -19,7 +20,7 @@ def sniffing_for_v(in_file):
 
 def metro_count_convert(in_filename,out_filename='default.xlsx'):
     z=pd.read_csv(in_filename,skiprows=sniffing_for_v(in_filename),header=None)
-    print('METRO COUNT',in_filename)
+    # print('METRO COUNT',in_filename)
     # print(z,z.columns,z[1].str.split(',',expand=True))
     z[['-a','Date']]=z[0].str.split(';',expand=True)
     z['time']=z[1]
@@ -27,13 +28,14 @@ def metro_count_convert(in_filename,out_filename='default.xlsx'):
     z['VClass']=z[3]
     z['Number of Axels']=z[4]
     z['Wheel Base']=z[5]
-    z['Quality Control']=z[6]
-    z=z.drop([0,1,2,3,4,5,6,'-a'],axis=1)
+    z[['Quality Control','b']]=z[6].str.split(';',expand=True)
+    z=z.drop([0,1,2,3,4,5,6,'-a','b'],axis=1)
+    z['CountType']='Metro'
     return z.to_excel(out_filename,index=False)
 
 def centurion_count_convert(in_filename,out_filename='default.xlsx'):
     z=pd.read_csv(in_filename,skiprows=sniffing_for_v(in_filename),header=None)
-    print('Centurion',in_filename,z)
+    # print('Centurion',in_filename,z)
     z[['-a','Date']]=z[0].str.split(';',expand=True)
     z['Time']=z[1]
     z[['Channel','Speed']]=z[2].str.split(';',expand=True)
@@ -43,8 +45,8 @@ def centurion_count_convert(in_filename,out_filename='default.xlsx'):
     # print('CENTURION ALMOST')
     col_list = ['Axle Length 1','Axle Length 2','Axle Length 3','Axle Length 4','Axle Length 5','Axle length 6','Axle Length 7', 'Axle Length 8', 'Axle Length 9', 'Axle Length 10', 'Axle Length 11', 'Axle Length 12']
     axle_len=z[6].str.split(';',expand=True)
-    # z[['Vclass1','i','Axle Length 1','Axle Length 2','Axle Length 3','Axle Length 4','Axle Length 5','Axle length 6','Axle Length 7']]=z[6].str.split(';',expand=True)
     
+    # z[['Vclass1','i','Axle Length 1','Axle Length 2','Axle Length 3','Axle Length 4','Axle Length 5','Axle length 6','Axle Length 7']]=z[6].str.split(';',expand=True)
     z['Vclass1']=axle_len[0]
     z['i']=axle_len[1]
 
@@ -54,30 +56,43 @@ def centurion_count_convert(in_filename,out_filename='default.xlsx'):
         z['Axle Length %d' % col] = axle_len[col + 2]
     z=z.drop(['-a',0,1,2,3,4,5,6],axis=1)
     z = z[z.columns[:-1]]
-    print("CENTURION FINISHED\n",z)
+    # print("CENTURION FINISHED\n",z)
+    z['CountType']='Centurion'
+
     return z.to_excel(out_filename,index=False)
 
-def geo_count_process(x):
-    onlyfiles = [os.path.join(mypath,f) for f in listdir(mypath) if isfile(join(mypath, f)) if f.endswith('.txt')]
+
+def geo_count_process(x,cent_number,metro_number):
+    # onlyfiles = [os.path.join(mypath,f) for f in listdir(mypath) if isfile(join(mypath, f)) if f.endswith('.txt')]
     term1='MetroCount'
     term2='Centurion-'
-    cent_number=1
-    metro_number=1
+    new_name=''
+    # cent_number=1
+    # metro_number=1
     # onlyfiles =[r'']
 
-    for a in onlyfiles:
+    # for a in onlyfiles:
         # print(a)
-        file=open(a).read().splitlines()
-        is_cent = 'Centurion' in file[3] and 'FV' in file[3]
-        is_metro = 'MetroCount' in file[3] and 'FV' in file[3]
-        if is_cent==True:
-            # new_name=r'hpms-site\static\geo_count_conversion\%s_%d.xlsx' % a, cent_number
-            # cent_number+=1
-            centurion_count_convert(a,out_filename='static/geo_count_conversion/geo_count_out.xlsx')
-        if is_metro==True:
-            # new_name=r'hpms-site\static\geo_count_conversion\%s_%d.xlsx' % a, metro_number
-            # metro_number+=1
-            metro_count_convert(a,out_filename='static/geo_count_conversion/geo_count_out.xlsx')
+    file=open(x).read().splitlines()
+    is_cent = 'Centurion' in file[3] and 'FV' in file[3]
+    is_metro = 'MetroCount' in file[3] and 'FV' in file[3]
+
+    if is_cent==True:
+        new_name=r'%s_%d.xlsx' % (x, cent_number)
+        cent_number+=1
+        centurion_count_convert(x,out_filename=new_name)
+    if is_metro==True:
+        new_name=r'%s_%d.xlsx' % (x, metro_number)
+        metro_number+=1
+        metro_count_convert(x,out_filename=new_name)
+
+    return (metro_number,cent_number,new_name)
+
+
+
+
+
+
     # for line in file:
     #     # line.strip().split('-')
     #     if term1 in line:
