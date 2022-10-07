56f976dc591041935d645ac8cadff597d7cf76fd
taylorjboone <taylorjamesboone@gmail.com>
2022-10-07 10:17:09 -0400

    added validations to sql table creation

--- pm/models/kortni_db.py
+++ pm/models/kortni_db.py
@@ -1,10 +1,22 @@
+from distutils.log import error
 from sqlalchemy import Float, create_engine, text, orm, MetaData, Table, Column, Integer, String, ForeignKey, Date
 from flask_sqlalchemy import SQLAlchemy
 from pm import db,app
 from wvdot_utils import check_seg_valid
+import pandas as pd
+import os
+import json
+import datetime
 
 
-engine = create_engine(r'sqlite+pysqlite:///C:\Users\E025205\Documents\GitHub\hpms-site\database.db', echo=True, future=True)
+user_dir = os.path.expanduser('~')
+engine = create_engine(f'sqlite+pysqlite:///{user_dir}\Documents\GitHub\hpms-site\database.db', echo=True, future=True)
+activity_codes = pd.read_csv(f'{user_dir}/Documents/GitHub/hpms-site/pm/static/activity_codes.csv')
+
+with open(f'{user_dir}/Documents/GitHub/hpms-site/pm/static/org_nums.json', 'r') as f:
+    org_nums = json.loads(f.read())
+
+
 metadata_obj = MetaData()
 
 mapper_registry = orm.registry()
@@ -42,16 +54,88 @@ class Task(Base):
         return '',True
 
     def validate(self):
-        # for func in self.func_list:
-        #     print(func(self))
-        print(self.validate_geom())
+        error_list = []
+
+        er0,bv0 = self.validate_geom()
+        error_list += [er0]
+        er1,bv1 = self.validate_org()
+        error_list += [er1]
+        er2,bv2 = self.validate_activity()
+        error_list += [er2]
+        er3,bv3 = self.validate_project_name()
+        error_list += [er3]
+        er4,bv4 = self.validate_accomplishments()
+        error_list += [er4]
+        er5,bv5 = self.validate_crew_members()
+        error_list += [er5]
+        er6,bv6 = self.validate_travel_hours()
+        error_list += [er6]
+        er7,bv7 = self.validate_onsite_hours()
+        error_list += [er7]
+        er8,bv8 = self.validate_task_date()
+        error_list += [er8]
+
+        return len(error_list)==0,error_list
+
+
+
+
+        
 
     def validate_geom(self):
         corrected_segs,message,bv = check_seg_valid(self.route_id, self.bmp, self.emp)
         print(corrected_segs)
-        if not bv:
+        if not bv and len(message) == 0:
             message = f'corrected segs are: {corrected_segs}'
+        elif bv and len(message) > 0: 
+            return message,bv 
         return message,bv
+
+    def validate_org(self):
+        # TODO populate org dict
+        if not self.org_num in org_nums.keys():
+            return f'Org Number: {self.org_num} not recognized',False
+        return '',True
+
+    def validate_activity(self):
+        if not self.activity_code in activity_codes['Activity Code']:
+            return f'Activity Code: {self.activity_code} not recognized',False
+        return '',True
+
+    def validate_project_name(self):
+        if self.project_name == None or len(self.project_name) == 0:
+            return f'Project name is empty',False
+        return '',True
+
+    def validate_accomplishments(self):
+        if self.accomplishments == None:
+            return 'Accomplishments field was left blank',False
+        return '',True
+
+    def validate_crew_members(self):
+        if self.crew_members == None:
+            return 'Crew members field was left blank',False
+        return '',True
+
+    def validate_travel_hours(self):
+        if self.travel_hours == None:
+            return 'Travel hours field was left blank',False
+        return '',True
+    
+    def validate_onsite_hours(self):
+        if self.onsite_hours == None:
+            return 'Onsite hours field was left blank',False
+        return '',True
+
+    def validate_task_date(self):
+        if not type(self.task_date) == datetime.date or not type(self.task_date) == datetime.datetime or self.task_date < datetime.date.today():
+            return 'Task Date not valid',False
+        return '',True
+
+    def add_task(self):
+        pass
+
+    
 
 
 
@@ -66,7 +150,8 @@ class Task(Base):
 
 
 t = Task()
-t.validate()
+print(t.validate())
+
 # with app.app_context():
 
 #     db.session.add(t)
